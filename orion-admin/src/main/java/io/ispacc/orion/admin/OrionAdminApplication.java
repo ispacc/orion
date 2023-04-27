1d6c01ae5068771c2909094c166705cb7518023a
十四 <yh.xion@outlook.com>
2023-04-27 19:02:27 +0800

    init

---
+++ orion-admin/src/main/java/io/ispacc/orion/admin/OrionAdminApplication.java
@@ -0,0 +1,13 @@
+package io.ispacc.orion.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrionAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrionAdminApplication.class, args);
+    }
+
+}
