b8296a9da2e78ed88bce4838d362de89a6f38de2
十四 <yh.xion@outlook.com>
2023-06-15 10:42:24 +0800

    feat: addCertificate and differentiateVersion

--- orion-admin/src/main/java/io/ispacc/orion/admin/module/admin/controller/AdminController.java
+++ orion-admin/src/main/java/io/ispacc/orion/admin/module/admin/controller/AdminController.java
@@ -6,16 +6,14 @@ import io.ispacc.orion.admin.module.admin.entity.User;
 import io.ispacc.orion.admin.module.admin.service.AdminService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("admin")
+@CrossOrigin("*")
 public class AdminController {
 
     @Value("${jwt.tokenHeader}")
